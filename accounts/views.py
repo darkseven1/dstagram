23aee41aeecd1b974b7ad0013d008380a62e5b5d
darkseven1 <darkseven1@naver.com>
2021-06-10 22:27:00 +0900

    Dstagram Service

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from .forms import RegisterForm
+
+
+def register(request):
+    if request.method == "POST":
+        user_form = RegisterForm(request.POST)
+        if user_form.is_valid():
+            new_user = user_form.save(commit=False)
+            new_user.set_password(user_form.cleaned_data['password'])
+            new_user.save()
+            return render(request, 'registration/register_done.html',{'new_user':new_user})
+    else:
+        # 회원 가입 내용을 입력하는 상황
+        user_form = RegisterForm()
+    return render(request, 'registration/register.html', {'form':user_form})
